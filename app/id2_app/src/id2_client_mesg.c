8c50df95bb462d3d07bea927b20da0f44656f810
security-devp <cedar.wxs@alibaba-inc.com>
2019-10-12 15:41:12 +0800

    Optimize id2 json message generation
    Signed-off-by: security-devp <cedar.wxs@alibaba-inc.com>

--- app/id2_app/src/id2_client_mesg.c
+++ app/id2_app/src/id2_client_mesg.c
@@ -3,7 +3,6 @@
  */
 
 #include "id2_test.h"
-#include "cJSON.h"
 
 /* parameters for id2 authcode */
 #define ID2_CHALLENGE       "55B83408399FA660F05C82E4F25333DC"
@@ -11,6 +10,37 @@
 #define ID2_EXTRA           "abcd1234"
 
 #define ID2_REPORT_VERSION      "1.0.0"
+
+#define ID2_JSON_MESSAGE  \
+"{\n\
+    \"reportVersion\":  \"%s\",\n\
+    \"sdkVersion\":  \"%s\",\n\
+    \"date\":  \"%s\",\n\
+    \"testContent\":  [{\n\
+                      \"api\":  \"id2_client_get_id\",\n\
+                      \"args\": {\n\
+                      },\n\
+                      \"result\":  \"%s\"\n\
+             }, {\n\
+                     \"api\":  \"id2_client_get_challenge_auth_code\",\n\
+                     \"args\": {\n\
+                              \"challenge\":  \"%s\",\n\
+                              \"extra\":      \"%s\"\n\
+                     },\n\
+                     \"result\": \"%s\"\n\
+             }, {\n\
+                     \"api\":  \"id2_client_get_timestamp_auth_code\",\n\
+                     \"args\": {\n\
+                              \"timestamp\":  \"%s\",\n\
+                              \"extra\":      \"%s\"\n\
+                     },\n\
+                     \"result\": \"%s\"\n\
+             }]\n\
+}"
+
+static uint8_t id2_id[ID2_ID_LEN + 1] = {0};
+static uint8_t auth_code_challenge[ID2_AUTH_CODE_BUF_LEN] = {0};
+static uint8_t auth_code_timestamp[ID2_AUTH_CODE_BUF_LEN] = {0};
 
 static int _char_to_hex(char c)
 {
@@ -54,74 +84,17 @@ static int _string_to_hex(char *str, uint32_t str_len, uint8_t *hex, uint32_t he
     return 0;
 }
 
-static int _generate_json_array(cJSON *json_root)
-{
-    int ret;
-    cJSON *json_array = NULL;
-    cJSON *json_node = NULL;
-    cJSON *json_args = NULL;
-    uint8_t id2_id[ID2_ID_LEN + 1] = {0};
-    uint32_t id2_len = ID2_ID_LEN;
-    uint8_t auth_code[ID2_AUTH_CODE_BUF_LEN] = {0};
-    uint32_t auth_code_len = ID2_AUTH_CODE_BUF_LEN;
-
-    cJSON_AddItemToObject(json_root, "testContent", json_array = cJSON_CreateArray());
-
-    /* id2_client_get_id */
-    cJSON_AddItemToArray(json_array, json_node = cJSON_CreateObject());
-    cJSON_AddStringToObject(json_node, "api", "id2_client_get_id");
-    cJSON_AddItemToObject(json_node, "args", cJSON_CreateObject());
-
-    ret = id2_client_get_id(id2_id, &id2_len);
-    if (ret != IROT_SUCCESS) {
-        ID2_DBG_LOG("id2 get id fail, %d\n", ret);
-        return -1;
-    }
-    cJSON_AddStringToObject(json_node, "result", (char *)id2_id);
-
-    /* id2_client_get_challenge_auth_code */
-    cJSON_AddItemToArray(json_array, json_node = cJSON_CreateObject());
-    cJSON_AddStringToObject(json_node, "api", "id2_client_get_challenge_auth_code");
-    cJSON_AddItemToObject(json_node, "args", json_args = cJSON_CreateObject());
-    cJSON_AddStringToObject(json_args, "challenge", ID2_CHALLENGE);
-    cJSON_AddStringToObject(json_args, "extra", ID2_EXTRA);
- 
-    ret = id2_client_get_challenge_auth_code(ID2_CHALLENGE,
-              (uint8_t *)ID2_EXTRA, strlen(ID2_EXTRA), auth_code, &auth_code_len);
-    if (ret != IROT_SUCCESS) {
-        ID2_DBG_LOG("id2 get challenge auth code fail, %d\n", ret);
-        return -1;
-    }
-    cJSON_AddStringToObject(json_node, "result", (char *)auth_code);
-
-    /* id2_client_get_timestamp_auth_code */
-    cJSON_AddItemToArray(json_array, json_node = cJSON_CreateObject());
-    cJSON_AddStringToObject(json_node, "api", "id2_client_get_timestamp_auth_code");
-    cJSON_AddItemToObject(json_node, "args", json_args = cJSON_CreateObject());
-    cJSON_AddStringToObject(json_args, "timestamp", ID2_TIMESTAMP);
-    cJSON_AddStringToObject(json_args, "extra", ID2_EXTRA);
-
-    auth_code_len = ID2_AUTH_CODE_BUF_LEN;
-    ret = id2_client_get_timestamp_auth_code(ID2_TIMESTAMP,
-              (uint8_t *)ID2_EXTRA, strlen(ID2_EXTRA), auth_code, &auth_code_len);
-    if (ret != IROT_SUCCESS) {
-        ID2_DBG_LOG("id2 get timestamp auth code fail, %d\n", ret);
-        return -1;
-    }
-    cJSON_AddStringToObject(json_node, "result", (char *)auth_code);
-
-    return 0;
-}
-
 int id2_client_generate_authcode(void)
 {
     int ret = 0;
     char *message = NULL;
-    cJSON *json_root = NULL;
     char vers_str[32] = {0};
-    char data_str[32] = {0};
+    char date_str[32] = {0};
     uint32_t version = 0;
+    uint32_t message_len = 0;
     uint32_t ver_max, ver_mid, ver_min;
+    uint32_t id2_len = ID2_ID_LEN;
+    uint32_t auth_code_len = ID2_AUTH_CODE_BUF_LEN;
 
     ID2_DBG_LOG("====> ID2 Client Generate AuthCode Start.\n");
 
@@ -141,29 +114,51 @@ int id2_client_generate_authcode(void)
     ver_mid = (version >>  8) & 0xFF;
     ver_min = (version >>  0) & 0xFF;
     ls_osa_snprintf(vers_str, 32, "%d.%d.%d", ver_max, ver_mid, ver_min);
-    ls_osa_snprintf(data_str, 32, "%s %s", __DATE__, __TIME__);
+    ls_osa_snprintf(date_str, 32, "%s %s", __DATE__, __TIME__);
 
-    json_root = cJSON_CreateObject();
-    if (NULL == json_root) {
-        ID2_DBG_LOG("create json object fail\n");
+    message_len = strlen(ID2_JSON_MESSAGE) + 16 + 32 + 32;
+
+    ret = id2_client_get_id(id2_id, &id2_len);
+    if (ret != IROT_SUCCESS) {
+        ID2_DBG_LOG("id2 get id fail, %d\n", ret);
         ret = -1;
         goto _out;
     }
+    message_len += id2_len;
 
-    cJSON_AddStringToObject(json_root, "reportVersion", ID2_REPORT_VERSION);
-    cJSON_AddStringToObject(json_root, "sdkVersion", vers_str);
-    cJSON_AddStringToObject(json_root, "date", data_str);
-
-    ret = _generate_json_array(json_root);
-    if (ret < 0) {
-        ID2_DBG_LOG("generate json array fail\n");
+    ret = id2_client_get_challenge_auth_code(ID2_CHALLENGE,
+              (uint8_t *)ID2_EXTRA, strlen(ID2_EXTRA), auth_code_challenge, &auth_code_len);
+    if (ret != IROT_SUCCESS) {
+        ID2_DBG_LOG("id2 get challenge auth code fail, %d\n", ret);
+        ret = -1;
         goto _out;
     }
+    message_len += auth_code_len;
 
-    message = cJSON_Print(json_root);
+    auth_code_len = ID2_AUTH_CODE_BUF_LEN;
+    ret = id2_client_get_timestamp_auth_code(ID2_TIMESTAMP,
+              (uint8_t *)ID2_EXTRA, strlen(ID2_EXTRA), auth_code_timestamp, &auth_code_len);
+    if (ret != IROT_SUCCESS) {
+        ID2_DBG_LOG("id2 get timestamp auth code fail, %d\n", ret);
+        ret = -1;
+        goto _out;
+    }
+    message_len += auth_code_len;
+
+    message = ls_osa_malloc(message_len);
     if (message == NULL) {
-        ID2_DBG_LOG("cjson print fail\n");
+        ID2_DBG_LOG("out of mem, %d\n", message_len);
         ret = -1;
+        goto _out;
+    }
+
+    ret = ls_osa_snprintf(message, message_len, ID2_JSON_MESSAGE,
+                          ID2_REPORT_VERSION, vers_str, date_str,
+                          id2_id,
+                          ID2_CHALLENGE, ID2_EXTRA, auth_code_challenge,
+                          ID2_TIMESTAMP, ID2_EXTRA, auth_code_timestamp);
+    if (ret < 0) {
+        ID2_DBG_LOG("id2 json message generation fail\n");
         goto _out;
     }
 
@@ -172,9 +167,6 @@ int id2_client_generate_authcode(void)
 _out:
     if (message != NULL) {
         ls_osa_free(message);
-    }
-    if (json_root != NULL) {
-        cJSON_Delete(json_root);
     }
 
     id2_client_cleanup();
